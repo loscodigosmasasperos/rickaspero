40d60aca5d582d71c2da2c350c9e2eda66de37da
AlvaroG <90227464+AlvaroG1016@users.noreply.github.com>
2023-01-20 14:25:20 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const btn = document.querySelector(".call");
-const btnC = document.querySelector(".clear");
-const main = document.querySelector("main");
+const btn = document.querySelector("#call");
+const main = document.createElement("main");
+const divCont = document.createElement('div');
+const body = document.querySelector("body");
+const buttons = document.querySelector(".buttons")
+divCont.className = "image-container";
+body.append(divCont);
 
 function callApi() {
     fetch("https://rickandmortyapi.com/api/character")
@@ -13,26 +17,57 @@ function callApi() {
 }
 
 function showPers(data) {
-    data.results.forEach(personaje => {//Aqui es donde estan todos los personajes (data.results)
+    data.results.forEach(character => {//Aqui es donde estan todos los personajes (data.results)
         const article = document.createRange().createContextualFragment(
-            `    <article>
+            `    <article class = "cont-Img">
 
-         <div class="image-container">
-             <img src= "${personaje.image}" alt="personaje"/>
+         <div class="card mb-3" style="max-width: 540px;">
+         <div class="row g-0">
+           <div class="col-md-4">
+             <img src="${character.image}" class="img-fluid rounded-start" alt="...">
+           </div>
+           <div class="col-md-8">
+             <div class="card-body">
+               <h5 class="card-title">${character.name}</h5>
+               <p class="card-text">${character.status}</p>
+               <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+             </div>
+           </div>
          </div>
-         <h2>${personaje.name}</h2>
-         <span>${personaje.status} </span>
+       </div>
+       
 
      </article> `
+     
         );
+     
+            
             main.append(article)
+            
         })
-
+        
+       
 }
+const limpiarBtn = document.createElement('button');
 
-btn.addEventListener("click", callApi);
-btnC.addEventListener("click", function(){
+
+btn.addEventListener("click", function(){
+    body.append(main)
+
+    callApi()
+
+    limpiarBtn.className = "btn btn-outline-light";
+    limpiarBtn.id = "clear"
+    limpiarBtn.textContent = "Limpiar"
+    buttons.append(limpiarBtn)
+    
+   
+});
+
+limpiarBtn.addEventListener("click", function(){
     main.innerHTML = '';
+    buttons.removeChild(limpiarBtn)
+    body.removeChild(main);
 })
 
 
